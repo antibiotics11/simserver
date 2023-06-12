b3629eca413806335e6e74c708c4793cf122b666
ANTIBIOTICS <abx@abx.pe.kr>
2023-06-12 20:40:10 +0900

    파라미터 기본값 변경
    setTimeZone의 timezone을 UTC에서 GMT로 변경,
    DateRFC2822의 timestamp를 -1에서 NUll로 변경

--- src/System/Time.php
+++ src/System/Time.php
@@ -4,13 +4,13 @@ namespace simserver\System;
 
 class Time {
 
-	public static function setTimeZone(String $timezone = "UTC"): String {
+	public static function setTimeZone(String $timezone = "GMT"): String {
 		date_default_timezone_set($timezone);
 		return date_default_timezone_get();
 	}
 
-	public static function DateRFC2822(int $timestamp = -1): String {
-		$timestamp = ($timestamp != -1) ? $timestamp : time();
+	public static function DateRFC2822(?int $timestamp = null): String {
+		$timestamp = $timestamp ?? time();
  		return sprintf("%s%s", substr(date(DATE_RFC2822, $timestamp), 0, -5), date("T"));
 	}
 
