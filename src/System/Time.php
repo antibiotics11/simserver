17ec41894c44532b2875748cd85f9d6a559c9e23
ANTIBIOTICS <abx@abx.pe.kr>
2023-06-14 11:43:15 +0900

    DateYMD 메소드 추가

--- src/System/Time.php
+++ src/System/Time.php
@@ -9,6 +9,11 @@ class Time {
 		return date_default_timezone_get();
 	}
 
+	public static function DateYMD(String $separator = "-", ?int $timestamp = null): String {
+		$timestamp = $timestamp ?? time();
+		return date(sprintf("Y%sm%sd", $separator, $separator), $timestamp);
+	}
+
 	public static function DateRFC2822(?int $timestamp = null): String {
 		$timestamp = $timestamp ?? time();
  		return sprintf("%s%s", substr(date(DATE_RFC2822, $timestamp), 0, -5), date("T"));
