497ec9703d844cbe5b079150093de863a7e6d7e8
ANTIBIOTICS <abx@abx.pe.kr>
2023-06-25 01:58:05 +0900

    parseRequest 함수 래픽토링
    필요한 문자/문자열은 멤버 상수를 사용,
    메시지 포맷을 조금 더 엄격하게 검사하도록 수정

--- src/Message/Message.php
+++ src/Message/Message.php
@@ -25,18 +25,18 @@ class Message {
 
   public static function parseRequest(String $data): Message {
 
-    $lines = explode(self::CRLF, $data);
-    if (count($lines) < 2) {
-      throw new \InvalidArgumentException("Invalid data format.");
+    $lines = explode(self::CHR_CR_LF, $data);
+    $linesCount = count($lines);
+    if ($linesCount < 2) {
+      throw new \InvalidArgumentException("Invalid message format.");
     }
 
     $message = new Message();
     $message->request = true;
-    $message->status = StatusCode::STATUS_OK;
 
-    $startLine = explode(chr(0x20), $lines[0]);
-    if (count($startLine) != 3) {
-      throw new \InvalidArgumentException("Invalid line format.");
+    $startLine = explode(self::CHR_BLANK, trim($lines[0]));
+    if (count($startLine) < 2 || count($startLine) > 3) {
+      throw new \InvalidArgumentException("Invalid start line format.");
     }
 
     $message->method = strtoupper($startLine[0]);
@@ -49,6 +49,7 @@ class Message {
       throw new \InvalidArgumentException("Invalid path.");
     }
 
+    $startLine[2] ??= self::DEFAULT_PROTOCOL;
     $message->protocol = strtoupper(trim($startLine[2]));
     if (strpos($message->protocol, "HTTP/") === false) {
       throw new \InvalidArgumentException("Invalid protocol.");
@@ -56,23 +57,29 @@ class Message {
 
     $message->header = [];
     $bodyIndex = 0;
-    for ($header = 1; $header < count($lines); $header++) {
+    for ($header = 1; $header < $linesCount; $header++) {
 
-      $headerName = "";
-      $headerValue = "";
+      $headerLine   = $lines[$header];
+      $headerLength = strlen($headerLine);
+      $headerName   = "";
+      $headerValue  = "";
       $tmp = "";
       $separatorFound = false;
 
-      for ($f = 0; $f < strlen($lines[$header]); $f++) {
-        if (ord($lines[$header][$f]) == 0x3a && !$separatorFound) {
+      if ($header == $linesCount -1 && $headerLength == 0) {
+        break;
+      }
+
+      for ($f = 0; $f < $headerLength; $f++) {
+        if (strcmp($headerLine[$f], self::CHR_COLON) == 0 && !$separatorFound) {
           $separatorFound = true;
           $headerName = trim($tmp);
           $tmp = "";
-          continue;
+        } else {
+          $tmp = sprintf("%s%s", $tmp, $headerLine[$f]);
         }
-        $tmp = sprintf("%s%s", $tmp, $lines[$header][$f]);
       }
-      $headerValue = $tmp;
+      $headerValue = trim($tmp);
 
       if (strlen($headerName) == 0 || strlen($headerValue) == 0) {
         throw new \InvalidArgumentException("Invalid header format.");
@@ -80,14 +87,20 @@ class Message {
 
       $message->header[$headerName] = $headerValue;
 
-      if (strlen($lines[$header + 1]) == 0) {
+      $nextLine = $lines[$header + 1] ?? "";
+      if (strcmp($nextLine, self::CHR_CR_LF) === 0 || strlen($nextLine) == 0) {
         $bodyIndex = $header + 2;
         break;
       }
 
     }
 
-    $message->body = implode(self::CRLF, array_slice($lines, $bodyIndex));
+    $message->body = "";
+    if ($bodyIndex > 0) {
+      $message->body = trim(implode(self::CHR_CR_LF,
+        array_slice($lines, $bodyIndex)
+      ));
+    }
 
     return $message;
 
