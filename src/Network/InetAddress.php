5c5f92f1b0d3da941cf2416218eeeed3969c0465
ANTIBIOTICS <abx@abx.pe.kr>
2023-07-07 16:33:47 +0900

    InetAddress 클래스 리팩토링
    객체를 직접 선언하지 않고 정적 메소드를 통해 반환받도록 수정,
    도메인 네임도 사용할수 있게 수정

--- src/Network/InetAddress.php
+++ src/Network/InetAddress.php
@@ -4,45 +4,89 @@ namespace simserver\Network;
 
 class InetAddress {
 
-	private String  $address;
-	private int     $family;
+	private String  $hostname      = "";           // Domain Name
+	private String  $address       = "";           // IP Address
+	private int     $addressFamily = AF_INET;      // IP Address Family
 
-	public function __construct(String $address = "") {
-		if (strlen($address) != 0) {
-			$this->setNewAddress($address);
-		}
-	}
-
-	public function setNewAddress(String $address): bool {
-
-		$address = strtolower(trim($address));
-		if (self::isIpv4($address)) {
-			$this->family = AF_INET;
-		} else if (self::isIpv6($address)) {
-			$this->family = AF_INET6;
-		} else {
-			return false;
-		}
-
-		$this->address = $address;
-		return true;
-
+	public function getHostname(): String {
+		return $this->hostname;
 	}
 
 	public function getAddress(): String {
 		return $this->address;
 	}
 
-	public function getFamily(): int {
-		return $this->family;
+	public function getAddressFamily(): int {
+		return $this->addressFamily;
 	}
 
-	public static function isIpv4(String $address): bool {
-		return filter_var($address, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4);
+	public static function getByHostname(String $hostname): ?self {
+		return self::getAllByHostname($hostname)[0] ?? null;
 	}
 
-	public static function isIpv6(String $address): bool {
-		return filter_var($address, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6);
+	public static function getAllByHostname(String $hostname): Array {
+
+		$dnsRecordA = @dns_get_record($hostname, DNS_A);
+		$dnsRecordAAAA = @dns_get_record($hostname, DNS_AAAA);
+		if ($dnsRecordA === false) {
+			$dnsRecordA = [];
+		}
+		if ($dnsRecordAAAA === false) {
+			$dnsRecordAAAA = [];
+		}
+		$dnsRecord = array_merge($dnsRecordA, $dnsRecordAAAA);
+
+		$inetAddresses = [];
+		foreach ($dnsRecord as $record) {
+			$ip = $record["ip"] ?? $record["ipv6"];
+			$inetAddress = self::getByAddress($ip);
+			$inetAddresses[] = $inetAddress;
+		}
+
+		return $inetAddresses;
+
+	}
+
+	public static function getByAddress(String $address): ?self {
+
+		$addressFamily = -1;
+		if (self::isIPv4($address)) {
+			$addressFamily = AF_INET;
+		} else if (self::isIPv6($address)) {
+			$addressFamily = AF_INET6;
+		} else {
+			return null;
+		}
+
+		$inetAddress = new self();
+		$inetAddress->address = $address;
+		$inetAddress->addressFamily = $addressFamily;
+
+		return $inetAddress;
+
+	}
+
+	public static function getByInput(String $input): ?self {
+
+		$target = strtolower(trim($input));
+		$inetAddress = null;
+
+		if (self::isIPv4($target) || self::isIPv6($target)) {
+			$inetAddress = self::getByAddress($target);
+		} else {
+			$inetAddress = self::getByHostname($target);
+		}
+
+		return $inetAddress;
+
+	}
+
+	public static function isIPv4(String $address): bool {
+		return filter_var($address, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4) !== false;
+	}
+
+	public static function isIPv6(String $address): bool {
+		return filter_var($address, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6) !== false;
 	}
 
 };
