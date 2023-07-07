4295cf4bc135ec7f17e5d8db6c443f062890ee36
ANTIBIOTICS <abx@abx.pe.kr>
2023-07-07 16:58:51 +0900

    PortChecker 클래스 리팩토링
    변경된 InetAddress 메소드 반영,
    소켓 bind에 성공했을때만 close하도록 수정,
    소켓 타입을 조금 더 엄격하게 처리

--- src/Network/PortChecker.php
+++ src/Network/PortChecker.php
@@ -4,32 +4,41 @@ namespace simserver\Network;
 
 class PortChecker {
 
-  public static function isPortInUse(int $port, InetAddress $address, int $protocol = \SOL_TCP): bool {
+  public static function validatePort(int $port): bool {
+    return ($port > 0 && $port <= 65535);
+  }
 
-    if (!self::validatePort($port, $address)) {
+  public static function isPortInUse(int $port, InetAddress $inetAddress, int $protocol = SOL_TCP): bool {
+
+    if (!self::validatePort($port)) {
       return false;
     }
-    return !self::isPortAvailable($port, $address, $protocol);
+    return !self::isPortAvailable($port, $inetAddress, $protocol);
 
   }
 
-  public static function isPortAvailable(int $port, InetAddress $address, int $protocol = \SOL_TCP): bool {
+  public static function isPortAvailable(int $port, InetAddress $inetAddress, int $protocol = SOL_TCP): bool {
 
-    $testSocketType = ($protocol == \SOL_TCP) ? \SOCK_STREAM : \SOCK_DGRAM;
-    $testSocket = @socket_create($address->getFamily(), $testSocketType, $protocol);
+    $address = $inetAddress->getAddress();
+    $addressFamily = $inetAddress->getAddressFamily();
+    $testSocketType = match ($protocol) {
+      SOL_TCP => SOCK_STREAM,
+      SOL_UDP => SOCK_DGRAM,
+      default => SOCK_RAW
+    };
+
+    $testSocket = @socket_create($addressFamily, $testSocketType, $protocol);
     if ($testSocket === false) {
       return false;
     }
 
-    $testBindingResult = @socket_bind($testSocket, $address->getAddress(), $port);
-    socket_close($testSocket);
+    $testBindingResult = @socket_bind($testSocket, $address, $port);
+    if ($testBindingResult) {
+      socket_close($testSocket);
+      return true;
+    }
+    return false;
 
-    return $testBindingResult;
-
-  }
-
-  public static function validatePort(int $port): bool {
-    return ($port > 0 && $port <= 65535);
   }
 
 };
