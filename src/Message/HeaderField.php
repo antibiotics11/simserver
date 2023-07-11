9954030101fef576361b04d1549818dd392ae217
ANTIBIOTICS <abx@abx.pe.kr>
2023-07-11 21:56:44 +0900

    enum으로 수정

--- src/Message/HeaderField.php
+++ src/Message/HeaderField.php
@@ -2,46 +2,46 @@
 
 namespace simserver\Message;
 
-class HeaderField {
+enum HeaderField: String {
 
   /**
    * General Header Fields
    */
-  public const HEADER_DATE               = "Date";
-  public const HEADER_PRAGMA             = "Pragma";
+  case DATE               = "Date";
+  case PRAGMA             = "Pragma";
 
   /**
    * Entity Header Fields
    */
-  public const HEADER_ALLOW              = "Allow";
-  public const HEADER_CONTENT_ENCODING   = "Content-Encoding";
-  public const HEADER_CONTENT_LENGTH     = "Content-Length";
-  public const HEADER_CONTENT_TYPE       = "Content-Type";
-  public const HEADER_EXPIRES            = "Expires";
-  public const HEADER_LAST_MODIFIED      = "Last-Modified";
-  public const HEADER_CONTENT_LANGUAGE   = "Content-Language";
-  public const HEADER_LINK               = "Link";
-  public const HEADER_TITLE              = "Title";
+  case ALLOW              = "Allow";
+  case CONTENT_ENCODING   = "Content-Encoding";
+  case CONTENT_LENGTH     = "Content-Length";
+  case CONTENT_TYPE       = "Content-Type";
+  case EXPIRES            = "Expires";
+  case LAST_MODIFIED      = "Last-Modified";
+  case CONTENT_LANGUAGE   = "Content-Language";
+  case LINK               = "Link";
+  case TITLE              = "Title";
 
   /**
    * Request Header Fields
    */
-  public const HEADER_AUTHORIZATION      = "Authorization";
-  public const HEADER_FROM               = "From";
-  public const HEADER_IF_MODIFIED_SINCE  = "If-Modified-Since";
-  public const HEADER_REFERER            = "Referer";
-  public const HEADER_USER_AGENT         = "User-Agent";
-  public const HEADER_ACCEPT             = "Accept";
-  public const HEADER_ACCEPT_CHARSET     = "Accept-Charset";
-  public const HEADER_ACCEPT_ENCODING    = "Accept-Encoding";
-  public const HEADER_ACCEPT_LANGUAGE    = "Accept-Language";
+  case AUTHORIZATION      = "Authorization";
+  case FROM               = "From";
+  case IF_MODIFIED_SINCE  = "If-Modified-Since";
+  case REFERER            = "Referer";
+  case USER_AGENT         = "User-Agent";
+  case ACCEPT             = "Accept";
+  case ACCEPT_CHARSET     = "Accept-Charset";
+  case ACCEPT_ENCODING    = "Accept-Encoding";
+  case ACCEPT_LANGUAGE    = "Accept-Language";
 
   /**
    * Response Header Fields
    */
-  public const HEADER_LOCATION           = "Location";
-  public const HEADER_SERVER             = "Server";
-  public const HEADER_WWW_AUTHENTICATE   = "WWW-Authenticate";
-  public const HEADER_RETRY_AFTER        = "Retry-After";
+  case LOCATION           = "Location";
+  case SERVER             = "Server";
+  case WWW_AUTHENTICATE   = "WWW-Authenticate";
+  case RETRY_AFTER        = "Retry-After";
 
 };
--- src/Message/StatusCode.php
+++ src/Message/StatusCode.php
@@ -2,44 +2,49 @@
 
 namespace simserver\Message;
 
-class StatusCode {
+enum StatusCode: int {
 
-  public const STATUS_OK                          = 200;
-  public const STATUS_CREATED                     = 201;
-  public const STATUS_ACCEPTED                    = 202;
-  public const STATUS_NO_CONTENT                  = 204;
+  case OK                          = 200;
+  case CREATED                     = 201;
+  case ACCEPTED                    = 202;
+  case NO_CONTENT                  = 204;
 
-  public const STATUS_MOVED_PERMANENTLY           = 301;
-  public const STATUS_MOVED_TEMPORARILY           = 302;
-  public const STATUS_NOT_MODIFIED                = 304;
+  case MOVED_PERMANENTLY           = 301;
+  case MOVED_TEMPORARILY           = 302;
+  case NOT_MODIFIED                = 304;
 
-  public const STATUS_BAD_REQUEST                 = 400;
-  public const STATUS_UNAUTHORIZED                = 401;
-  public const STATUS_FORBIDDEN                   = 403;
-  public const STATUS_NOT_FOUND                   = 404;
+  case BAD_REQUEST                 = 400;
+  case UNAUTHORIZED                = 401;
+  case FORBIDDEN                   = 403;
+  case NOT_FOUND                   = 404;
 
-  public const STATUS_INTERNAL_SERVER_ERROR       = 500;
-  public const STATUS_NOT_IMPLEMENTED             = 501;
-  public const STATUS_BAD_GATEWAY                 = 502;
-  public const STATUS_SERVICE_UNAVAILABLE         = 503;
+  case INTERNAL_SERVER_ERROR       = 500;
+  case NOT_IMPLEMENTED             = 501;
+  case BAD_GATEWAY                 = 502;
+  case SERVICE_UNAVAILABLE         = 503;
 
-  public static function toMessage(int $statusCode): String {
+  public static function toMessage(int | self $statusCode): String {
+
+    if ($statusCode instanceof self) {
+      $statusCode = $statusCode->value;
+    }
+
     switch ($statusCode) {
-      case self::STATUS_OK                    : return "OK";
-      case self::STATUS_CREATED               : return "Created";
-      case self::STATUS_ACCEPTED              : return "Accepted";
-      case self::STATUS_NO_CONTENT            : return "No Content";
-      case self::STATUS_MOVED_PERMANENTLY     : return "Moved Permanently";
-      case self::STATUS_MOVED_TEMPORARILY     : return "Moved Temporarily";
-      case self::STATUS_NOT_MODIFIED          : return "Not Modified";
-      case self::STATUS_BAD_REQUEST           : return "Bad Request";
-      case self::STATUS_UNAUTHORIZED          : return "Unauthorized";
-      case self::STATUS_FORBIDDEN             : return "Forbidden";
-      case self::STATUS_NOT_FOUND             : return "Not Found";
-      case self::STATUS_INTERNAL_SERVER_ERROR : return "Internal Server Error";
-      case self::STATUS_NOT_IMPLEMENTED       : return "Not Implemented";
-      case self::STATUS_BAD_GATEWAY           : return "Bad Gateway";
-      case self::STATUS_SERVICE_UNAVAILABLE   : return "Service Unavailable";
+      case self::OK->value                    : return "OK";
+      case self::CREATED->value               : return "Created";
+      case self::ACCEPTED->value              : return "Accepted";
+      case self::NO_CONTENT->value            : return "No Content";
+      case self::MOVED_PERMANENTLY->value     : return "Moved Permanently";
+      case self::MOVED_TEMPORARILY->value     : return "Moved Temporarily";
+      case self::NOT_MODIFIED->value          : return "Not Modified";
+      case self::BAD_REQUEST->value           : return "Bad Request";
+      case self::UNAUTHORIZED->value          : return "Unauthorized";
+      case self::FORBIDDEN->value             : return "Forbidden";
+      case self::NOT_FOUND->value             : return "Not Found";
+      case self::INTERNAL_SERVER_ERROR->value : return "Internal Server Error";
+      case self::NOT_IMPLEMENTED->value       : return "Not Implemented";
+      case self::BAD_GATEWAY->value           : return "Bad Gateway";
+      case self::SERVICE_UNAVAILABLE->value   : return "Service Unavailable";
       default                                 : return "";
     };
   }
