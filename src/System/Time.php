6d7accf2b2f631657a4f903f9dffdc8c26f5cfb6
ANTIBIOTICS <abx@abx.pe.kr>
2023-07-11 21:52:29 +0900

    연산자 변경

--- src/System/Time.php
+++ src/System/Time.php
@@ -10,12 +10,12 @@ class Time {
 	}
 
 	public static function DateYMD(String $separator = "-", ?int $timestamp = null): String {
-		$timestamp = $timestamp ?? time();
+		$timestamp ??= time();
 		return date(sprintf("Y%sm%sd", $separator, $separator), $timestamp);
 	}
 
 	public static function DateRFC2822(?int $timestamp = null): String {
-		$timestamp = $timestamp ?? time();
+		$timestamp ??= time();
  		return sprintf("%s%s", substr(date(DATE_RFC2822, $timestamp), 0, -5), date("T"));
 	}
 
