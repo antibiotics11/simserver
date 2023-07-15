06bff63482db1b629719b750dff8d2f85677501a
ANTIBIOTICS <abx@abx.pe.kr>
2023-07-15 13:09:08 +0900

    switch를 match문으로 변경, 주석 추가

--- src/Http/StatusCode.php
+++ src/Http/StatusCode.php
@@ -4,49 +4,56 @@ namespace simserver\Http;
 
 enum StatusCode: int {
 
-  case OK                          = 200;
-  case CREATED                     = 201;
-  case ACCEPTED                    = 202;
-  case NO_CONTENT                  = 204;
+  case OK                          = 200;  // Ok
+  case CREATED                     = 201;  // Created
+  case ACCEPTED                    = 202;  // Accepted
+  case NO_CONTENT                  = 204;  // No Content
 
-  case MOVED_PERMANENTLY           = 301;
-  case MOVED_TEMPORARILY           = 302;
-  case NOT_MODIFIED                = 304;
+  case MOVED_PERMANENTLY           = 301;  // Moved Permanently
+  case MOVED_TEMPORARILY           = 302;  // Moved Temporarily (Found in HTTP/1.1)
+  case NOT_MODIFIED                = 304;  // Not Modified
 
-  case BAD_REQUEST                 = 400;
-  case UNAUTHORIZED                = 401;
-  case FORBIDDEN                   = 403;
-  case NOT_FOUND                   = 404;
+  case BAD_REQUEST                 = 400;  // Bad Request
+  case UNAUTHORIZED                = 401;  // Unauthorized
+  case FORBIDDEN                   = 403;  // Forbidden
+  case NOT_FOUND                   = 404;  // Not Found
 
-  case INTERNAL_SERVER_ERROR       = 500;
-  case NOT_IMPLEMENTED             = 501;
-  case BAD_GATEWAY                 = 502;
-  case SERVICE_UNAVAILABLE         = 503;
+  case INTERNAL_SERVER_ERROR       = 500;  // Internal Server Error
+  case NOT_IMPLEMENTED             = 501;  // Not Implemented
+  case BAD_GATEWAY                 = 502;  // Bad Gateway
+  case SERVICE_UNAVAILABLE         = 503;  // Service Unavailable
 
+  /**
+   * Converts status code to its corresponding message.
+   *
+   * @param int|self $statusCode status code
+   * @return string  status message
+   */
   public static function toMessage(int | self $statusCode): String {
 
     if ($statusCode instanceof self) {
       $statusCode = $statusCode->value;
     }
 
-    switch ($statusCode) {
-      case self::OK->value                    : return "OK";
-      case self::CREATED->value               : return "Created";
-      case self::ACCEPTED->value              : return "Accepted";
-      case self::NO_CONTENT->value            : return "No Content";
-      case self::MOVED_PERMANENTLY->value     : return "Moved Permanently";
-      case self::MOVED_TEMPORARILY->value     : return "Moved Temporarily";
-      case self::NOT_MODIFIED->value          : return "Not Modified";
-      case self::BAD_REQUEST->value           : return "Bad Request";
-      case self::UNAUTHORIZED->value          : return "Unauthorized";
-      case self::FORBIDDEN->value             : return "Forbidden";
-      case self::NOT_FOUND->value             : return "Not Found";
-      case self::INTERNAL_SERVER_ERROR->value : return "Internal Server Error";
-      case self::NOT_IMPLEMENTED->value       : return "Not Implemented";
-      case self::BAD_GATEWAY->value           : return "Bad Gateway";
-      case self::SERVICE_UNAVAILABLE->value   : return "Service Unavailable";
-      default                                 : return "";
+    return match ($statusCode) {
+      self::OK->value                    => "OK",
+      self::CREATED->value               => "Created",
+      self::ACCEPTED->value              => "Accepted",
+      self::NO_CONTENT->value            => "No Content",
+      self::MOVED_PERMANENTLY->value     => "Moved Permanently",
+      self::MOVED_TEMPORARILY->value     => "Moved Temporarily",
+      self::NOT_MODIFIED->value          => "Not Modified",
+      self::BAD_REQUEST->value           => "Bad Request",
+      self::UNAUTHORIZED->value          => "Unauthorized",
+      self::FORBIDDEN->value             => "Forbidden",
+      self::NOT_FOUND->value             => "Not Found",
+      self::INTERNAL_SERVER_ERROR->value => "Internal Server Error",
+      self::NOT_IMPLEMENTED->value       => "Not Implemented",
+      self::BAD_GATEWAY->value           => "Bad Gateway",
+      self::SERVICE_UNAVAILABLE->value   => "Service Unavailable",
+      default                                 => ""
     };
+
   }
 
 };
