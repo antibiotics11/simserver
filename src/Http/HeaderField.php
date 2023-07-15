591755f8a097658b9b22a5fda5148c4477bd090c
ANTIBIOTICS <abx@abx.pe.kr>
2023-07-15 15:08:22 +0900

    HTTP/1.1 캐시 헤더 추가, tryFromStr 메소드 추가

--- src/Http/HeaderField.php
+++ src/Http/HeaderField.php
@@ -37,11 +37,36 @@ enum HeaderField: String {
   case ACCEPT_LANGUAGE    = "Accept-Language";
 
   /**
+   * HTTP/1.1+ Cache Headers
+   */
+  case ETAG               = "ETag";
+  case IF_MATCH           = "If-Match";
+  case IF_NONE_MATCH      = "If-None-Match";
+
+  /**
    * Response Header Fields
    */
   case LOCATION           = "Location";
   case SERVER             = "Server";
   case WWW_AUTHENTICATE   = "WWW-Authenticate";
   case RETRY_AFTER        = "Retry-After";
+
+
+  /**
+   * Tries to convert a http header field string to corresponding HeaderField instance.
+   *
+   * @param string $headerFieldStr http header field string to convert
+   * @return HeaderField|null corresponding HeaderField instance, or null if no match is found
+   */
+  public static function tryFromStr(String $headerFieldStr): ?self {
+
+    foreach (self::cases() as $headerField) {
+      if (strcasecmp($headerFieldStr, $headerField->value)) {
+        return $headerField;
+      }
+    }
+    return null;
+
+  }
 
 };
