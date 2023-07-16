46df93537cb20a6ad9ae1fc70f381fef6e4ebf04
ANTIBIOTICS <abx@abx.pe.kr>
2023-07-16 13:37:42 +0900

    Time 리팩토링
    메소드명 변경: DateRFC2822() => DateRFC7231(),
    메소드 추가: DateRFC2822(), getTimeZone(), DateCookie()

--- src/System/Time.php
+++ src/System/Time.php
@@ -4,19 +4,31 @@ namespace simserver\System;
 
 class Time {
 
-	public static function setTimeZone(String $timezone = "GMT"): String {
-		date_default_timezone_set($timezone);
-		return date_default_timezone_get();
-	}
+  public static function setTimeZone(String $timezone = "GMT"): void {
+    date_default_timezone_set($timezone);
+  }
 
-	public static function DateYMD(String $separator = "-", ?int $timestamp = null): String {
-		$timestamp ??= time();
-		return date(sprintf("Y%sm%sd", $separator, $separator), $timestamp);
-	}
+  public static function getTimeZone(): String {
+    return date_default_timezone_get();
+  }
 
-	public static function DateRFC2822(?int $timestamp = null): String {
-		$timestamp ??= time();
- 		return sprintf("%s%s", substr(date(DATE_RFC2822, $timestamp), 0, -5), date("T"));
-	}
+  public static function DateYMD(String $separator = "-", ?int $timestamp = null): String {
+    return date(sprintf("Y%sm%sd", $separator, $separator), $timestamp ?? time());
+  }
+
+  // Formats given timestamp as a date string in RFC2822 format. (current time in default)
+  public static function DateRFC2822(?int $timestamp = null): String {
+    return date(DATE_RFC2822, $timestamp ?? time());
+  }
+
+  // Formats given timestamp as a date string in RFC7231 format. (current time in default)
+  public static function DateRFC7231(?int $timestamp = null): String {
+    return date(DATE_RFC7231, $timestamp ?? time());
+  }
+
+  // Formats given timestamp as a date string in HTTP Cookie format.
+  public static function DateCookie(int $timestamp): String {
+    return date(DATE_COOKIE, $timestamp);
+  }
 
 };
